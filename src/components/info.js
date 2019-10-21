bfec0bd26b7d6f3d571cffa40a29766f132f5599
Taufik Nurrohman <t.nurrohman77@gmail.com>
2019-10-21 23:34:35 +0700

    Add Color Option

--- src/components/info.js
+++ src/components/info.js
@@ -1,13 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Info({ text, href, target }) {
+function Info({ text, href, target, color }) {
   return (
-    <a className="block p-2 bg-orange-600 hover:bg-orange-700 text-orange-100 border-b border-orange-800 hover:border-orange-900 text-center" href={href} target={target} role="alert">{text}</a>
+    <a className={`block p-2 bg-${color}-600 hover:bg-${color}-700 text-${color}-100 border-b border-${color}-800 hover:border-${color}-900 text-center`} href={href} target={target} role="alert">{text}</a>
   );
 }
 
 Info.defaultProps = {
+  color: 'blue',
   href: '/',
   target: '_self',
   text: 'Hey!'
