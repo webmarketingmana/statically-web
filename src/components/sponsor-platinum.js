bb8f092f3c95a16f3488b0d0c9d019637a6c53af
Frans Allen <frans@upset.dev>
2020-01-22 19:00:35 +0700

    Rearranged Platinum sponsor logos

--- src/components/sponsor-platinum.js
+++ src/components/sponsor-platinum.js
@@ -3,30 +3,6 @@ import React from "react";
 function SponsorPlatinum() {
   return (
     <div className="flex content-center items-center max-w-xl mx-auto flex-wrap">
-      <div className="w-full sm:w-1/2 md:w-1/5 lg:w-1/5 md:ml-3 md:mr-2 p-1">
-        <a
-          href="https://www.cdn77.com"
-          className="sponsors-logo"
-          target="_blank"
-        >
-          <img
-            src={'/images/sponsors/cdn77.svg'}
-          />
-        </a>
-      </div>
-
-      <div className="w-full sm:w-1/2 md:w-1/4 lg:w-1/4 p-1">
-        <a
-          href="https://bunnycdn.com"
-          className="sponsors-logo"
-          target="_blank"
-        >
-          <img
-            src={'/images/sponsors/bunnycdn.svg'}
-          />
-        </a>
-      </div>
-
       <div className="w-full sm:w-1/2 md:w-1/4 lg:w-1/4 md:-mr-3 p-1">
         <a
           href="https://www.fastly.com"
@@ -47,6 +23,30 @@ function SponsorPlatinum() {
         >
           <img
             src={'/images/sponsors/cloudflare.svg'}
+          />
+        </a>
+      </div>
+      
+      <div className="w-full sm:w-1/2 md:w-1/4 lg:w-1/4 p-1">
+        <a
+          href="https://bunnycdn.com"
+          className="sponsors-logo"
+          target="_blank"
+        >
+          <img
+            src={'/images/sponsors/bunnycdn.svg'}
+          />
+        </a>
+      </div>
+
+      <div className="w-full sm:w-1/2 md:w-1/5 lg:w-1/5 md:ml-3 md:mr-2 p-1">
+        <a
+          href="https://www.cdn77.com"
+          className="sponsors-logo"
+          target="_blank"
+        >
+          <img
+            src={'/images/sponsors/cdn77.svg'}
           />
         </a>
       </div>
